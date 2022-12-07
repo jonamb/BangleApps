56b0c914241d2f55741d401d9a3268db8fcfd08e
jonamb <jonamb@jam-world.de>
2022-12-07 22:00:44 +0100

    Update app.js

--- apps/annoy/app.js
+++ apps/annoy/app.js
@@ -6,13 +6,12 @@ settings = require("Storage").readJSON(FILE, true);
 if (settings.act) {
   settings.act = false;
   Bangle.buzz(320);
+  require("Storage").writeJSON(FILE, settings);
 } else {
   settings.act = true;
   delay = 60 * (settings.itv + Math.random() * settings.itr);
   settings.nxt = Math.round(Date.now() / 1000 + delay);
-  Bangle.buzz(80);
-  setTimeout(Bangle.buzz, 150, 80);
-  setTimeout(Bangle.buzz, 300, 80);
+  require("Storage").writeJSON(FILE, settings);
+  require("buzz").pattern(settings.buz).then(() => {Bangle.showClock;});
 }
-require("Storage").writeJSON(FILE, settings);
 setTimeout(Bangle.showClock, 350);
