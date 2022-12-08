77d6d638f7d0a2a15856cc0607ecef7961a46fcc
jonamb <jonamb@jam-world.de>
2022-12-08 19:38:50 +0100

    Update app.js
    Renamed settings to annset

--- apps/annoy/app.js
+++ apps/annoy/app.js
@@ -1,17 +1,16 @@
 // App
 
 FILE = "annoy.json";
-settings = require("Storage").readJSON(FILE, true);
+annset = require("Storage").readJSON(FILE, true);
 
-if (settings.act) {
-  settings.act = false;
-  Bangle.buzz(320);
-  require("Storage").writeJSON(FILE, settings);
+if (annset.act) {
+  annset.act = false;
+  require("Storage").writeJSON(FILE, annset);
+  Bangle.buzz(320).then(Bangle.showClock);
 } else {
-  settings.act = true;
-  delay = 60 * (settings.itv + Math.random() * settings.itr);
-  settings.nxt = Math.round(Date.now() / 1000 + delay);
-  require("Storage").writeJSON(FILE, settings);
-  require("buzz").pattern(settings.buz).then(() => {Bangle.showClock;});
+  annset.act = true;
+  delay = 60 * (annset.itv + Math.random() * annset.itr);
+  annset.nxt = Math.round(Date.now() / 1000 + delay);
+  require("Storage").writeJSON(FILE, annset);
+  require("buzz").pattern(", ,").then(Bangle.showClock);
 }
-setTimeout(Bangle.showClock, 350);
